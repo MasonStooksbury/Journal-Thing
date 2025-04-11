ccd68d1e8a20ae090eb540e057b05b54faf8db0c
Mason Stooksbury <masonstooksbury@gmail.com>
2025-04-10 23:33:20 -0500

    Adding more fluff

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,8 @@
-import preactLogo from '../../assets/preact.svg';
 import './style.css';
 
 export function Home() {
 	return (
 		<div class="home">
-			{/* <a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>*/}
-			{/* <h1>Get Started building Vite-powered Preact Apps </h1> */}
-
 			<div class='grid'>
 		    <ProgressTracker />
 			  <TextArea />
@@ -17,7 +11,7 @@ export function Home() {
 	);
 }
 
-function TextArea(props) {
+function TextArea() {
   return (
     <div class='text-container'>
       <textarea class='text'/>
@@ -28,31 +22,30 @@ function TextArea(props) {
   );
 }
 
-function ProgressTracker(props) {
+function ProgressTracker() {
+  let now = new Date();
+  const currentYear = now.getFullYear();
+
   const array = [];
-  // const array = [<p>S</p>,<p>M</p>,<p>T</p>,<p>W</p>,<p>R</p>,<p>F</p>,<p>S</p>,];
-  for (let i = 0; i < 365; i++) {
-    array.push(<div class='tiny-box' title={dateFromDay(2025, i)}/>)
+  for (let i = 1; i < 366; i++) {
+    array.push(<div class='tiny-box' title={dateFromDay(currentYear, i)}/>)
   }
   return (
-    <div class='box-container'>
-      {array}
+    <div class='progress-tracker'>
+      <div class='year-row'>
+        <a class='previous'>&lt;</a>
+        <span class='year-label'>{currentYear}</span>
+        <a class='next'>&gt;</a>
+      </div>
+      <div class='box-container'>
+        {array}
+      </div>
     </div>
   );
 }
 
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
-}
-
-
 function dateFromDay(year: number, day: number) {
   let date = new Date(year, 0); // initialize a date in `year-01-01`
-  let newDate = new Date(date.setDate(day+1)); // add the number of days
+  let newDate = new Date(date.setDate(day)); // add the number of days
   return `${newDate.toLocaleString('default', { month: 'long' })} ${newDate.getDate()}`;
 }
